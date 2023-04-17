d67a037672a9ca241c5e3083089662663e59e992
snowbanana12345 <50855780+snowbanana12345@users.noreply.github.com>
2023-04-17 16:57:12 +0800

    add length validation test to TickData

---
+++ sandbox/scratch.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+x = np.array([[4.0, 5.0, 3.0, 2.0, 1.0], [5.0,6.0,4.0,3.0,2.0]], dtype = float)
+print(x.shape)
+print(x.shape[0])
+print(len(x.shape))
--- src/nutshellml/model/data_model.py
+++ src/nutshellml/model/data_model.py
@@ -11,3 +11,32 @@ class TickData:
     bid_sizes : np.array
     ask_prices : np.array
     ask_sizes : np.array
+
+    def __post_init__(self):
+        if len(self.timestamps.shape) != 1:
+            raise ValueError("timestamp array should be 1d")
+        if len(self.trade_prices.shape) != 1:
+            raise ValueError("trade prices array should be 1d")
+        if len(self.trade_sizes.shape) != 1:
+            raise ValueError("trade sizes array should be 1d")
+        if len(self.bid_prices.shape) != 2:
+            raise ValueError("bid prices should be a 2d array")
+        if len(self.bid_sizes.shape) != 2:
+            raise ValueError("bid sizes should be a 2d array")
+        if len(self.ask_prices.shape) != 2:
+            raise ValueError("ask prices should be a 2d array")
+        if len(self.ask_sizes.shape) != 2:
+            raise ValueError("ask sizes should be a 2d array")
+        length = self.timestamps.shape[0]
+        if self.trade_prices.shape[0] != length:
+            raise ValueError(f"trade sizes array should be the same length as timestamp : {length}")
+        if self.trade_sizes.shape[0] != length:
+            raise ValueError(f"trade sizes array should be the same length as timestamp : {length}")
+        if self.bid_prices.shape[1] != length:
+            raise ValueError(f"bid prices array should be the same length as timestamp : {length}")
+        if self.ask_prices.shape[1] != length:
+            raise ValueError(f"ask prices array should be the same length as timestamp : {length}")
+        if self.bid_sizes.shape[1] != length:
+            raise ValueError(f"bid sizes array should be the same length as timestamp : {length}")
+        if self.ask_sizes.shape[1] != length:
+            raise ValueError(f"ask sizes array should be the same length as timestamp : {length}")
